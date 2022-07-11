977663692bf8a172cbc5c22ca223de28c513a7ee
puradak <bbln2252@naver.com>
2022-07-11 15:57:18 +0900

    7월 11일 월요일 스터디(2)

--- src/tetris/Block.java
+++ src/tetris/Block.java
@@ -1,8 +1,20 @@
 package tetris;
 
+import java.awt.Color;
+
 public class Block {
+	
+	
+	public Block(int x, int y, Color c) {
+		this.X = x;
+		this.Y = y;
+		this.color = c;
+	}
 	private int X;
 	private int Y;
+	// 중심 좌표 : 사각형 생성 위치는 중심-20
+	private Color color;
+	
 	
 	public void setXY(int x, int y) {
 		this.X = x;
@@ -18,4 +30,13 @@ public class Block {
 	public int getY() {
 		return this.Y;
 	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	
 }

927de32773bf14745a692c91b5beb8af2a1ab506
jung0115 <jungim7490@gmail.com>
2022-07-16 16:08:49 +0900

    회전(블럭 크기 고려)

--- src/tetris/Play_gui.java
+++ src/tetris/Play_gui.java
@@ -5,23 +5,21 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Play_gui {
-
 	static private Block blocks[] = new Block[4];
 	// 블럭 뭉치
 
-	static private int blockColumn = 2;        // 블럭이 차지하는 n*m 공간이 회전 될 때마다 세로가 2, 4 중 하나로 변경되므로 그 정보를 저장
-	static private int blockStart[] = {1, 2};  // 블럭이 차지하는 n*m 좌상단 좌표를 항상 기억
+	static private int blockColumn = 2;            // 블럭이 차지하는 n*m 공간이 회전 될 때마다 세로가 2, 4 중 하나로 변경되므로 그 정보를 저장
+	static private int blockStart[] = {60, 100};      // 블럭이 차지하는 n*m 좌상단 좌표를 항상 기억
+	static private final int blockSize = 40;       // 블럭 크기 상수로 지정
 	
 	static private int location[][] = new int[20][10];
 	// 블럭 위치 저장  : 메서드가 필요
 	
 	static private int shape[][][] = {
-
 			{{1,1,1,1},{0,0,0,0}/*,{0,0,0,0},{0,0,0,0}*/}
 	}; 
 	// 7개
 	
-
 	static private Color color[] = {
 			Color.RED,
 			Color.ORANGE,
@@ -52,44 +50,34 @@ public class Play_gui {
 		scorePanel.setBounds(400,0,200,800);
 		scorePanel.setBackground(Color.GRAY);
 		frame.add(scorePanel);
-		
-		rotationRight();
 	}
 	
 	// 2차원 배열을 시계방향으로 90도 회전하기
-	// 회전이 되는 축: blockStart[0], blockStart[1]
-	// blocks[]에 현재 선택된 블럭의 객체가 들어있음 -> 객체가 자리하는 위치를 n*m 배열로 나타낸 뒤, 회전시켜서 m*n 배열로 변경
-	// 변경됨 m*n 배열에서 자리를 차지하고 있는 부분이 객체의 위치이므로 객체의 x, y 좌표 정보를 회전 이후 좌표로 변경
+	// 회전이 되는 축: blockStart[0], blockStart[1] -> 블럭을 이동할 때마다 블럭이 위치하는 n*m 공간의 좌상단 좌표를 blockStart[]에 업데이트 해야함
+	// 회전 메서드에서는 회전 후의 좌표로 x, y 값 변경만하므로 변경된 좌표를 읽어서 GUI에 적용하는 작업도 필요함
 	static public void rotationRight() {
-		int testXY[][] = {{1, 2}, {1,3}, {2, 3}, {2, 4}};
-		int change[][] = new int[(blockColumn % 4) + 2][blockColumn];
-		
+		/*int testXY[][] = {{60, 100}, {60, 140}, {100, 140}, {100, 180}};
+		int change[][] = new int[(blockColumn % 4) + 2][blockColumn];*/
+
     	for(int i = 0; i < 4; i++) {
-    		// int changeX = blocks[i].getY() + blockStart[0] - blockStart[1];
-    		// int changeY = blockColumn - 1 - blocks[i].getX() + blockStart[0] + blockStart[1];
-    		// blocks[i].setXY(changeX, changeY);
+    		int changeX = blocks[i].getY() + blockStart[0] - blockStart[1];
+    		int changeY = blockColumn * blockSize - blockSize - blocks[i].getX() + blockStart[0] + blockStart[1];
+    		blocks[i].setXY(changeX, changeY);
     		
-    		change[i][0] = testXY[i][1] + blockStart[0] - blockStart[1];
-    		change[i][1] = blockColumn - 1 - testXY[i][0] + blockStart[0] + blockStart[1];
+    		//change[i][0] = testXY[i][1] + blockStart[0] - blockStart[1];
+    		//change[i][1] = blockColumn * blockSize - blockSize - testXY[i][0] + blockStart[0] + blockStart[1];
     	}
     	blockColumn = (blockColumn % 4) + 2; // 회전 되었으므로 세로 길이 변경
     	
-    	for(int i = 0; i < 4; i++) {
+    	/*for(int i = 0; i < 4; i++) {
     		System.out.println(change[i][0] + " " + change[i][1]);
-    	}
-    	
-        /*int[][] ret = new int[key.length][key[0].length];
-        for(int i=0; i<key[0].length; i++) {
-            for(int j=0; j<key.length; j++) {
-                // 오른쪽으로 90도 회전
-                ret[i][j] = key[key.length-1-j][i];
-            }
-        }*/
+    	}*/
     }
     
     
 	private void setBlock(int shapeNum) {
 		blockColumn = 2; // 블럭의 세로 범위는 2로 초기값을 줌. 블럭 생성 시마다 초기화
+    	
     	int[][] selected = shape[shapeNum];
     	int row = -1; int col = -1;
     	int count = 0;
@@ -114,7 +102,6 @@ public class Play_gui {
     // 모든 블럭의 위치를 저장한다.
     
 	private void printBlock(/*blocks 이용*/) {
-
     	
     }
 }
